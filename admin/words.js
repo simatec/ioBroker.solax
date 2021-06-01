3a2d40d239abbcf0b1f2dcf0d64bb078232933a4
simatec <nais@gmx.net>
2021-06-01 16:24:50 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'solax adapter settings': {
+        'en': 'Adapter settings for solax',
+        'de': 'Adaptereinstellungen für solax',
+        'ru': 'Настройки адаптера для solax',
+        'pt': 'Configurações do adaptador para solax',
+        'nl': 'Adapterinstellingen voor solax',
+        'fr': "Paramètres d'adaptateur pour solax",
+        'it': "Impostazioni dell'adattatore per solax",
+        'es': 'Ajustes del adaptador para solax',
+        'pl': 'Ustawienia adaptera dla solax',
+        'zh-cn': 'solax的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
